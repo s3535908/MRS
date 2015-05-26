c07de6647c0d4f2707e20a260d1b027872e3e551
Mads Svendsen <s3535908@student.rmit.edu.au>
2015-05-26 17:44:55 +1000

    Initial Commit

---
+++ PosturePlus/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PosturePlus
+//
+//  Created by Mads Svendsen on 26/05/15.
+//  Copyright (c) 2015 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
